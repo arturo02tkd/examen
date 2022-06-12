0967aa734276527516a3a8b385fa1450ee765eee
arturo02tkd <arturo02tkd@gmail.com>
2022-06-12 16:54:17 -0500

    inicio
    se crea rama para examen mercado libre

---
+++ Hermandad/src/main/java/com/mx/hermandad/HermandadApplication.java
@@ -0,0 +1,13 @@
+package com.mx.hermandad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermandadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HermandadApplication.class, args);
+    }
+ 
+}
